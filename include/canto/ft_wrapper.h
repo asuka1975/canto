2ed76214758a13ffa9e94d3f00613be3f89d74d8
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2021-07-12 03:28:52 +0900

    feat: make freetype wrapper classes

--- include/canto/ft_wrapper.h
+++ include/canto/ft_wrapper.h
@@ -5,4 +5,23 @@
 #ifndef CANTO_FT_WRAPPER_H
 #define CANTO_FT_WRAPPER_H
 
+#include <memory>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+namespace canto {
+    struct library : std::unique_ptr<FT_Library, void(*)(FT_Library*)> {
+        FT_Library operator->();
+        static library& get_library() noexcept;
+    private:
+        library();
+    };
+    
+    struct face : std::unique_ptr<FT_Face, void(*)(FT_Face*)> {
+        face(FT_Library& library, const std::string& font_file, FT_Long index);
+        FT_Face operator->();
+    };
+}
+
 #endif //CANTO_FT_WRAPPER_H
--- src/ft_wrapper.cpp
+++ src/ft_wrapper.cpp
@@ -2,3 +2,30 @@
 // Created by hungr on 2021/07/12.
 //
 #include "canto/ft_wrapper.h"
+
+FT_Library canto::library::operator->() {
+    return *get();
+}
+
+canto::library &canto::library::get_library() noexcept {
+    static library lib{};
+    return lib;
+}
+
+canto::library::library() : std::unique_ptr<FT_Library, void(*)(FT_Library *)>(new FT_Library, +[](FT_Library* ptr) {
+    FT_Done_FreeType(*ptr);
+    delete ptr;
+}) {
+    FT_Init_FreeType(get());
+}
+
+canto::face::face(FT_Library &library, const std::string &font_file, FT_Long index) : std::unique_ptr<FT_Face, void(*)(FT_Face *)>(new FT_Face, +[](FT_Face* ptr) {
+    FT_Done_Face(*ptr);
+    delete ptr;
+}) {
+    FT_New_Face(library, font_file.c_str(), index, get());
+}
+
+FT_Face canto::face::operator->() {
+    return *get();
+}
